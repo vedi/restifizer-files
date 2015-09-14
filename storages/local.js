568147e8347751cb78912eb3ea04b1cd58ec33e5
vedi <shubin.fedor@gmail.com>
2015-09-14 09:49:51 +0300

    implement local storage

---
+++ storages/local.js
@@ -0,0 +1,46 @@
+'use strict';
+/**
+ * Created by vedi on 09/14/15.
+ */
+var
+  Promise = require('bluebird'),
+  fs = Promise.promisifyAll(require('fs-extra')),
+  pathM = require('path'),
+  crypto = require('crypto');
+
+module.exports = {
+
+  initialize: function(options) {
+    this.uploadPath = options.uploadPath + '/';
+    this.db = options.ModelClass.db.db;
+  },
+
+  getFileAsync: function (fileMeta) {
+    var fileName = fileMeta;
+    return fs.openAsync(fileName, 'r');
+  },
+
+  putFileAsync: function (path, options) {
+    var buf = crypto.randomBytes(64);
+    var fileName = this.uploadPath + buf.toString('hex') + '.' + pathM.extname();
+
+    return fs.moveAsync(path, fileName)
+      .then(function () {
+        return fileName;
+      });
+  },
+
+  replaceFileAsync: function (fileMeta, path, options) {
+    var fileName = fileMeta;
+    return fs.moveAsync(path, fileName, {clobber: true})
+      .then(function () {
+        return fileName;
+      });
+  },
+
+  deleteFileAsync: function (fileMeta) {
+    var fileName = fileMeta;
+    return fs.unlinkAsync(fileName);
+  }
+
+};
